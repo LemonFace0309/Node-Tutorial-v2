10d1c5b0201ad4d3073b234aabe1184d4b7ce863
Charles Liu <charles.liu0309@gmail.com>
2021-04-11 04:50:35 -0400

    added product crud functionality and connected user to cart with a custom method

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const errorController = require('./controllers/error')
+const User = require('./models/user')
 
 const app = express()
 
@@ -17,14 +18,14 @@ const shopRoutes = require('./routes/shop')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use((req, res, next) => {
-//   User.findByPk('5baa2528563f16379fc8a610')
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch((err) => console.log(err))
-// })
+app.use((req, res, next) => {
+  User.findById('6072a792f323cc22801c8abd')
+    .then((user) => {
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log(err))
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -32,16 +33,25 @@ app.use(shopRoutes)
 app.use(errorController.get404)
 
 mongoose
-  .connect(
-    process.env.MONGOOSE_URI,
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGOOSE_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Charles',
+          email: 'test@test.com',
+          cart: {
+            items: [],
+          },
+        })
+        user.save()
+      }
+    })
     app.listen(3000)
   })
   .catch((err) => {
--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     imageUrl: imageUrl,
     description: description,
+    userId: req.user._id, //_id not required, mongosse can store the user id with just req.user
   })
   product
     .save()
@@ -38,7 +39,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId
   Product.findById(prodId)
-    // Product.findById(prodId)
     .then((product) => {
       if (!product) {
         return res.redirect('/')
@@ -60,15 +60,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl
   const updatedDesc = req.body.description
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  )
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle
+      product.price = updatedPrice
+      product.imageUrl = updatedImageUrl
+      product.description = updatedDesc
+      return product.save()
+    })
     .then((result) => {
       console.log('UPDATED PRODUCT!')
       res.redirect('/admin/products')
@@ -77,7 +76,9 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
+    //.populate('userId') provides the full user object instead of just the user ID
+    // .select('title price -_id') only provides the selected fields with id explicitly excluded
     .then((products) => {
       res.render('admin/products', {
         prods: products,
@@ -90,7 +91,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT')
       res.redirect('/admin/products')
