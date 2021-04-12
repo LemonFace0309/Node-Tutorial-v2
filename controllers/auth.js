a8ec6d10c4448af73d28e3806c6806e32e1f1caa
Charles Liu <charles.liu0309@gmail.com>
2021-04-12 06:02:07 -0400

    grinded out some errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,6 @@ const User = require('../models/user')
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn =
   //   req.get('Cookie').split(';')[1].trim().split('=')[1] == true
-  console.log(req.session)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -19,7 +18,12 @@ exports.postLogin = (req, res, next) => {
       // present in all requests
       req.session.isLoggedIn = true
       req.session.user = user
-      res.redirect('/')
+      // usually unnecessary but we added it so res.redirect('/')
+      //   doesn't occur before req.session finishes sending data to the store
+      req.session.save(err => {
+        console.log(err)
+        res.redirect('/')
+      }) 
     })
     .catch((err) => console.log(err))
 }
