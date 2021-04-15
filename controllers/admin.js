bbd810bc0933f222946ad1070e8a66fef061b22f
Charles Liu <charles.liu0309@gmail.com>
2021-04-15 06:58:27 -0400

    added user authorization for admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -67,21 +67,23 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
+      if (product.userId.toString() !== req.user._id.toString()) {
+        return res.redirect('/')
+      }
       product.title = updatedTitle
       product.price = updatedPrice
       product.imageUrl = updatedImageUrl
       product.description = updatedDesc
-      return product.save()
-    })
-    .then((result) => {
-      console.log('UPDATED PRODUCT!')
-      res.redirect('/admin/products')
+      return product.save().then((result) => {
+        console.log('UPDATED PRODUCT!')
+        res.redirect('/admin/products')
+      })
     })
     .catch((err) => console.log(err))
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  Product.find({ userId: req.user._id })
     //.populate('userId') provides the full user object instead of just the user ID
     // .select('title price -_id') only provides the selected fields with id explicitly excluded
     .then((products) => {
@@ -97,9 +99,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.findByIdAndRemove(prodId)
+  // Product.findByIdAndRemove(prodId)
+  Product.deleteOne({id: prodId, userId: req.user._id})
     .then(() => {
-      console.log('DESTROYED PRODUCT')
       res.redirect('/admin/products')
     })
     .catch((err) => console.log(err))
